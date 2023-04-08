b0ef747737d24d9647849ab81ec85a83e6d5d521
offical-waqas-khurshid <waqaskhurshidoffical@gmail.com>
2023-04-08 13:46:39 +0500

    add project

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:quiz_app_flutter/screens/welcome/welcome_screen.dart';
+
+void main() {
+  runApp( MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Quiz App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: WelcomeScreen(),
+    );
+  }
+}
---
+++ lib/screens/quiz/components/body.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import '../../../constants.dart';
+import '../../../controllers/question_controller.dart';
+import 'progress_bar.dart';
+import 'question_card.dart';
+
+class Body extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    QuestionController _questionController = Get.put(QuestionController());
+    return Stack(
+      children: [
+        SvgPicture.asset("assets/icons/bg.svg", fit: BoxFit.fill),
+        SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+               Padding(
+                padding:
+                     EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                child: ProgressBar(),
+              ),
+              SizedBox(height: kDefaultPadding),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                child: Obx(
+                  () => Text.rich(
+                    TextSpan(
+                      text:
+                          "Question ${_questionController.questionNumber.value}",
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline4!
+                          .copyWith(color: kSecondaryColor),
+                      children: [
+                        TextSpan(
+                          text: "/${_questionController.questions.length}",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline5!
+                              .copyWith(color: kSecondaryColor),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Divider(thickness: 1.5),
+              SizedBox(height: kDefaultPadding),
+              Expanded(
+                child: PageView.builder(
+                  // Block swipe to next qn
+                  physics: NeverScrollableScrollPhysics(),
+                  controller: _questionController.pageController,
+                  onPageChanged: _questionController.updateTheQnNum,
+                  itemCount: _questionController.questions.length,
+                  itemBuilder: (context, index) => QuestionCard(
+                      question: _questionController.questions[index]),
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
